312f373c649abde14d05ce7099b855e815b8eeca
gnumobin <gnumobin@gmail.com>
2025-03-11 16:51:21 +0330

    Create Header Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Route, Routes } from 'react-router'
 import './App.scss'
+import HeadNav from './components/HeadNav/HeadNav'
 
 function App() {
   return <>
+    <HeadNav />
     <Routes>
       <Route />
     </Routes>
--- src/components/HeadNav/HeadNav.jsx
+++ src/components/HeadNav/HeadNav.jsx
@@ -1,8 +1,31 @@
+import { Link } from 'react-router';
+// Custom Imports
 import './HeadNav.scss'
+import Logo from '../../assets/img/logo.png'
 
 const HeadNav = _ => {
     return <nav className="head-nav">
-    
+        <div className="head-nav__logo-box">
+           <a href="#">
+            <img src={Logo} alt="Brand Logo" className='head-nav__logo'/>
+           </a>
+        </div>
+        <ul className="head-nav__list">
+            <li className="head-nav__item">
+                <Link className='head-nav__link'>Sobre</Link>
+            </li>
+            <li className="head-nav__item">
+                <Link className='head-nav__link'>Contato</Link>
+            </li>
+            <li className="head-nav__item">
+                <Link className='head-nav__link'>Anunciar</Link>
+            </li>
+            <li className="head-nav__item">
+                <Link className='head-nav__link head-nav__link--active'>
+                    Estoque
+                </Link>
+            </li>
+        </ul>
     </nav>
 }
 
