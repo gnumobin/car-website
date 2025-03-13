4abb9052532aee2bccbc9ab184bd877f2f4c076b
gnumobin <gnumobin@gmail.com>
2025-03-13 11:35:54 +0330

    move information component to productHero, deploy app to the github pages

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,9 +1,14 @@
 import './ProductPage.scss'
 import Information from '../../components/Information/Information';
+import ProductHero from '../../components/ProductHero/ProductHero';
+import CTAForm from '../../components/CTAForm/CTAForm';
+import Footer from '../../components/Footer/Footer';
 
 const ProductPage = _ => {
     return <section className="section-product">
-        <Information />
+        <ProductHero />
+        <CTAForm />
+        <Footer />
     </section>
 }
 
